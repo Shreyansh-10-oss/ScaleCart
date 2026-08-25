7c1663cfaa915d885c6a7e2798495d3d217d2be2
Shreyansh-10-oss <shreyanshagarwal004@gmail.com>
2026-08-26 00:25:29 +0530

    Initial Spring Boot project setup

---
+++ scalecart/src/main/java/com/scalecart/ScalecartApplication.java
@@ -0,0 +1,13 @@
+package com.scalecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalecartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalecartApplication.class, args);
+	}
+
+}
---
+++ scalecart/src/test/java/com/scalecart/ScalecartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalecartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
